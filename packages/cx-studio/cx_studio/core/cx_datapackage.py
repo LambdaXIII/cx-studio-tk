573ee0b5c4f02eefc1407263b874969dc649301c
xiii_1991 <xiii_1991@163.com>
2025-03-30 17:41:31 +0800

    bug fix

--- packages/cx-studio/cx_studio/core/cx_datapackage.py
+++ packages/cx-studio/cx_studio/core/cx_datapackage.py
@@ -1,47 +1,68 @@
+from collections import defaultdict
+from typing import Union
+
+
 class DataPackage:
     """_summary_
-        数据包装类，用于包装数据，支持字典和属性访问
+    数据包装类，用于包装数据，支持字典和属性访问
     """
-    def def __init__(self):
-        self.__data = {}
+
+    def __init__(self, **kwargs):
+        self.__data = defaultdict(DataPackage)
+        for k, v in kwargs.items():
+            self.__data[k] = DataPackage.__check_value(v)
+
+    def update(self, other: Union[dict, "DataPackage"]):
+        if not isinstance(other, (dict, DataPackage)):
+            raise TypeError("other must be dict or DataPackage")
+        data = (
+            other
+            if isinstance(other, DataPackage)
+            else DataPackage.__check_value(other)
+        )
+        self.__data.update(data.__data)
 
     @classmethod
-    def from_dict(cls,data:dict):
+    def from_dict(cls, data: dict):
         result = cls()
-        for k,v in data.items():
+        for k, v in data.items():
             result.__data[k] = DataPackage.__check_value(v)
 
     @staticmethod
-    def __check_values(value):
-        if isinstance(value,list):
-            for value in values:
-                return [ DataPackage.__check_values(v) for v in value ]
-        elif isinstance(value,dict):
+    def __check_value(value):
+        if isinstance(value, list):
+            return [DataPackage.__check_value(v) for v in value]
+        elif isinstance(value, dict):
             return DataPackage.from_dict(value)
         else:
             return value
 
-    def __getattr__(self,key):
+    def __getattr__(self, key):
         return self._data[key]
 
-    def __setattr__(self,key,value):
+    def __setattr__(self, key, value):
         self._data[key] = DataPackage.__check_value(value)
 
-    def __getitem__(self,key):
-        if isinstance(key, str) and ('.' in key):
-            keys = key.split('.')
+    def __getitem__(self, key):
+        if isinstance(key, str) and ("." in key):
+            keys = key.split(".")
             value = self._data[keys[0]]
             for k in keys[1:]:
                 value = value[k]
             return value
         return self._data[key]
 
-    def __setitem__(self,key,value):
-        if isinstance(key, str) and ('.' in key):
-            keys = key.split('.')
+    def __setitem__(self, key, value):
+        if isinstance(key, str) and ("." in key):
+            keys = key.split(".")
             d = self._data[keys[0]]
             for k in keys[1:-1]:
                 d = d[k]
             d[keys[-1]] = DataPackage.__check_value(value)
         else:
             self._data[key] = DataPackage.__check_value(value)
+
+    def __rich_repr__(self):
+        yield "DataPackage"
+        for k, v in self._data.items():
+            yield k, v
