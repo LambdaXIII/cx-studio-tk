d175156043c5f76c843590c0318dc2d7b970a5c7
xiii_1991 <xiii_1991@163.com>
2025-04-05 03:13:50 +0800

    typing fix

--- packages/cxalio-studio-tools/media_killer/application.py
+++ packages/cxalio-studio-tools/media_killer/application.py
@@ -11,7 +11,7 @@ from .components import Preset
 
 
 class Application(IApplication):
-    def __init__(self, arguments: Sequence[str] | None=None):
+    def __init__(self, arguments: Sequence[str] | None = None):
         super().__init__(arguments or sys.argv[1:])
 
     def start(self):
@@ -26,29 +26,31 @@ class Application(IApplication):
     def export_example_preset(filename: Path):
         filename = Path(PathUtils.force_suffix(filename, ".toml"))
         if filename.exists():
-            if (
-                    appenv.context.force_overwrite 
-                    and not appenv.context.force_no_overwrite 
-            ):
+            if appenv.context.force_overwrite and not appenv.context.force_no_overwrite:
                 appenv.say("文件已存在，[red]将覆盖目标文件！[/red]")
             else:
                 appenv.say("[red]文件已存在[/red]，请指定其它文件名！")
                 return
 
         with importlib.resources.open_text(
-                "media_killer", "example_preset.toml"
+            "media_killer", "example_preset.toml"
         ) as example:
             with open(filename, "w", encoding="utf-8") as f:
                 f.write(example.read())
 
-        appenv.say(
-            f"已生成示例配置文件：[yellow]{filename}[yellow]。[red]请在修改后使用！[/red]"
-        )
+        appenv.say(f"已生成示例配置文件：{filename}。[red]请在修改后使用！[/red]")
 
     def run(self):
         if appenv.context.generate:
             for s in appenv.context.sources:
-                self.export_example_preset(s)
+                s = Path(s)
+                suffix = s.suffix
+                if suffix == ".toml" or suffix == "":
+                    self.export_example_preset(s)
+                else:
+                    appenv.whisper(
+                        "{filename} 并非合法的文件名，不予处理。".format(filename=s)
+                    )
             return
 
         appenv.whisper("Making missions from sources...")
